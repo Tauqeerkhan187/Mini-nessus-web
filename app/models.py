f7df802fe80c98cdf38c4d1986ef73cd1057e1f5
TRK_187 <trk_187@kali>
2026-03-03 18:15:33 -0500

    model file for app

---
+++ app/models.py
@@ -0,0 +1,36 @@
+# Author:TK
+# Date: 03-03-2026
+# Purpose: models used in webapp
+
+from datetime import datetime
+from . import db
+
+class Scan(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    target = db.Column(db.String(255), nullable=False)
+    ports = db.Column(db.String(255), nullable=False) # example "22, 80, 443"
+    profile = db.Column(db.String(50), nullable=False, default="quick")
+    status = db.Column(db.String(50), nullable=False, default="queued") # queued/running/done/failed
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    started_at = db.Column(db.DateTime, nullable=True)
+    finished_at = db.Column(db.DateTime, nullable=True)
+    error = db.Column(db.Text, nullable=True)
+
+    celery_task_id = db.Column(db.String(128), nullable=True)
+
+    findings = db.relationship("Finding", backref="scan", cascade="all, delete-orphan", lazy=True)
+
+class Finding(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    scan_id = db.column(db.Integer, db.ForeignKey("scan.id"), nullable=False)
+
+    port = db.Column(db.Integer, nullable=True)
+    service = db.Column(db.String(100), nullable=True)
+    banner = db.Column(db.Text, nullable=True)
+
+    issue = db.Column(db.String(255), nullable=False)
+    severity = db.Column(db.String(20), nullable=False) # LOW, MED, HIGH, CRICTICAL
+    recommendation = db.Column(db.Text, nullable=False)
+
+    
+
