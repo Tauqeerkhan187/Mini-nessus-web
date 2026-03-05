e9c415aa7ad5e5fb95b9127418326db82555227e
TRK_187 <trk_187@kali>
2026-03-04 20:57:12 -0500

    refactored model.py

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 # Author:TK
 # Date: 03-03-2026
-# Purpose: models used in webapp
+# Purpose: db schema definition, scan for scan job, finiding for individual vulnerabilities
 
 from datetime import datetime
 from . import db
@@ -11,26 +11,25 @@ class Scan(db.Model):
     ports = db.Column(db.String(255), nullable=False) # example "22, 80, 443"
     profile = db.Column(db.String(50), nullable=False, default="quick")
     status = db.Column(db.String(50), nullable=False, default="queued") # queued/running/done/failed
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    started_at = db.Column(db.DateTime, nullable=True)
-    finished_at = db.Column(db.DateTime, nullable=True)
-    error = db.Column(db.Text, nullable=True)
+   created_at = db.Column(db.DateTime, default=datetime.utcnow)
+   started_at = db.Column(db.DateTime, nullable=True)
+   finished_at = db.Column(db.DateTime, nullable=True)
+   error = db.Column(db.Text, nullable=True)
 
-    celery_task_id = db.Column(db.String(128), nullable=True)
+   celery_task_id = db.Column(db.String(128), nullable=True)
 
-    findings = db.relationship("Finding", backref="scan", cascade="all, delete-orphan", lazy=True)
+   findings = db.relationship("Finding", backref="scan", cascade="all, delete-orphan", lazy=True)
 
 class Finding(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    scan_id = db.column(db.Integer, db.ForeignKey("scan.id"), nullable=False)
+    scan_id = db.Column(db.Integer, db.ForeignKey("scan.id"), nullable=False)
 
     port = db.Column(db.Integer, nullable=True)
     service = db.Column(db.String(100), nullable=True)
     banner = db.Column(db.Text, nullable=True)
 
     issue = db.Column(db.String(255), nullable=False)
-    severity = db.Column(db.String(20), nullable=False) # LOW, MED, HIGH, CRICTICAL
+    severity = db.Column(db.String(20), nullable=False) # LOW/MEDIUM/HIGH/CRITICAL
     recommendation = db.Column(db.Text, nullable=False)
 
-    
 
